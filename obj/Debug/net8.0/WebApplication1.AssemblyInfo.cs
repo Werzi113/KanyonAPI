5e5df88f753a2a3ec4bd66637a5238a0f58568f9
MagitechnicUstrelPrdel <matykvapil00@gmail.com>
2025-05-06 00:18:39 +0200

    First commit

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f5784b40a0a4762b81174e31527539fff2d7592")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afbb74fc964ccf0c448c476545e2910429224535")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
